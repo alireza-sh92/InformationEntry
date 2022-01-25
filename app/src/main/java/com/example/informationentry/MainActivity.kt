222c051a1d425524cb841e11424992b499d8543f
Alireza <asg.1371@gmail.com>
2022-01-26 00:43:45 +0330

    pushed!

---
+++ app/src/main/java/com/example/informationentry/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.informationentry
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.core.graphics.blue
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
